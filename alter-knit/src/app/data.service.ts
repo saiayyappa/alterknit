4ce82f37625e23e2bca93618e3b3187128dff4b9
Sai <saiayyappaor@gmail.com>
2021-11-17 22:18:47 +0530

    commented dummy order data

--- alter-knit/src/app/data.service.ts
+++ alter-knit/src/app/data.service.ts
@@ -118,121 +118,121 @@ export class Order {
   providedIn: 'root'
 })
 export class DataService {
-  // order = new Order();
-  order: Order = {
-    "orderMethod": 1,
-    "garments": [
-      {
-        "serviceNeeded": [
-          {
-            "name": "Reknitting of holes on your sweaters, hats, gloves, scarves, blanket, exc...",
-            "checked": false
-          },
-          {
-            "name": "Reweaving for suits, blazers, slacks, overcoat, exc...",
-            "checked": false
-          },
-          {
-            "name": "Reknitting of",
-            "checked": false
-          },
-          {
-            "name": "Knit Alteration",
-            "checked": false
-          },
-          {
-            "name": "Knit Restyling",
-            "checked": true
-          },
-          {
-            "name": "Depilling",
-            "checked": false
-          },
-          {
-            "name": "Cleaning",
-            "checked": false
-          },
-          {
-            "name": "Custom Knit Work",
-            "checked": true
-          },
-          {
-            "name": "Other Fabric Repair",
-            "checked": false
-          }
-        ],
-        "isDryCleaned": false,
-        "isCleaned": false,
-        "brand": "J Crew",
-        "color": "red",
-        "ageOfGarment": 1,
-        "noOfHoles": 1,
-        "briefDescription": "This is a red color j crew sweater."
-      },
-      {
-        "serviceNeeded": [
-          {
-            "name": "Reknitting of holes on your sweaters, hats, gloves, scarves, blanket, exc...",
-            "checked": false
-          },
-          {
-            "name": "Reweaving for suits, blazers, slacks, overcoat, exc...",
-            "checked": false
-          },
-          {
-            "name": "Reknitting of",
-            "checked": false
-          },
-          {
-            "name": "Knit Alteration",
-            "checked": false
-          },
-          {
-            "name": "Knit Restyling",
-            "checked": false
-          },
-          {
-            "name": "Depilling",
-            "checked": false
-          },
-          {
-            "name": "Cleaning",
-            "checked": true
-          },
-          {
-            "name": "Custom Knit Work",
-            "checked": false
-          },
-          {
-            "name": "Other Fabric Repair",
-            "checked": true
-          }
-        ],
-        "isDryCleaned": false,
-        "isCleaned": false,
-        "brand": "Gucci",
-        "color": "pink",
-        "ageOfGarment": 1,
-        "noOfHoles": 1,
-        "briefDescription": "This is a pink color sweater from gucci."
-      }
-    ],
-    "addressInfo": {
-      firstName: 'Sai',
-      lastName: 'Ayyappa',
-      buildingType: 1,
-      address: 'alsdkflasdkf lasdjlfkjasdf asdf',
-      city: 'Madurai',
-      state: 'TN',
-      zipcode: '625012',
-      phone: '9999999999',
-      email: 'saiayyappaor@gmail.com',
-      pickUpDate: '',
-      pickUpTime: '',
-      isBillingAddress: false
-    },
-    "deliverySpeed": 0
-  }
+  order = new Order();
+  // order: Order = {
+  //   "orderMethod": 1,
+  //   "garments": [
+  //     {
+  //       "serviceNeeded": [
+  //         {
+  //           "name": "Reknitting of holes on your sweaters, hats, gloves, scarves, blanket, exc...",
+  //           "checked": false
+  //         },
+  //         {
+  //           "name": "Reweaving for suits, blazers, slacks, overcoat, exc...",
+  //           "checked": false
+  //         },
+  //         {
+  //           "name": "Reknitting of",
+  //           "checked": false
+  //         },
+  //         {
+  //           "name": "Knit Alteration",
+  //           "checked": false
+  //         },
+  //         {
+  //           "name": "Knit Restyling",
+  //           "checked": true
+  //         },
+  //         {
+  //           "name": "Depilling",
+  //           "checked": false
+  //         },
+  //         {
+  //           "name": "Cleaning",
+  //           "checked": false
+  //         },
+  //         {
+  //           "name": "Custom Knit Work",
+  //           "checked": true
+  //         },
+  //         {
+  //           "name": "Other Fabric Repair",
+  //           "checked": false
+  //         }
+  //       ],
+  //       "isDryCleaned": false,
+  //       "isCleaned": false,
+  //       "brand": "J Crew",
+  //       "color": "red",
+  //       "ageOfGarment": 1,
+  //       "noOfHoles": 1,
+  //       "briefDescription": "This is a red color j crew sweater."
+  //     },
+  //     {
+  //       "serviceNeeded": [
+  //         {
+  //           "name": "Reknitting of holes on your sweaters, hats, gloves, scarves, blanket, exc...",
+  //           "checked": false
+  //         },
+  //         {
+  //           "name": "Reweaving for suits, blazers, slacks, overcoat, exc...",
+  //           "checked": false
+  //         },
+  //         {
+  //           "name": "Reknitting of",
+  //           "checked": false
+  //         },
+  //         {
+  //           "name": "Knit Alteration",
+  //           "checked": false
+  //         },
+  //         {
+  //           "name": "Knit Restyling",
+  //           "checked": false
+  //         },
+  //         {
+  //           "name": "Depilling",
+  //           "checked": false
+  //         },
+  //         {
+  //           "name": "Cleaning",
+  //           "checked": true
+  //         },
+  //         {
+  //           "name": "Custom Knit Work",
+  //           "checked": false
+  //         },
+  //         {
+  //           "name": "Other Fabric Repair",
+  //           "checked": true
+  //         }
+  //       ],
+  //       "isDryCleaned": false,
+  //       "isCleaned": false,
+  //       "brand": "Gucci",
+  //       "color": "pink",
+  //       "ageOfGarment": 1,
+  //       "noOfHoles": 1,
+  //       "briefDescription": "This is a pink color sweater from gucci."
+  //     }
+  //   ],
+  //   "addressInfo": {
+  //     firstName: 'Sai',
+  //     lastName: 'Ayyappa',
+  //     buildingType: 1,
+  //     address: 'alsdkflasdkf lasdjlfkjasdf asdf',
+  //     city: 'Madurai',
+  //     state: 'TN',
+  //     zipcode: '625012',
+  //     phone: '9999999999',
+  //     email: 'saiayyappaor@gmail.com',
+  //     pickUpDate: '',
+  //     pickUpTime: '',
+  //     isBillingAddress: false
+  //   },
+  //   "deliverySpeed": 0
+  // }
   orderSubject = new BehaviorSubject(this.order);
   constructor() { }
 
