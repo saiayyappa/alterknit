0e042b1f6067496ef1b67512a0fe4fba8644b36b
Sai Ayyappa <saiayyappaor@gmail.com>
2022-01-26 00:20:17 +0530

    Update app.component.ts
    Set current authenticated user on app start

--- alter-knit/src/app/app.component.ts
+++ alter-knit/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'alter-knit';
+
+  constructor(private auth: AuthService) {
+    this.auth.getCurrentAuthenticatedUser();
+  }
+
 }
